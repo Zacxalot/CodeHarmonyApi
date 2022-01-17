f5ddf108a106a008ef2fede6bad7cf068e13bea5
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-01-17 00:01:32 +0000

    Now decoding the url values

--- src/lesson_session.rs
+++ src/lesson_session.rs
@@ -1,5 +1,6 @@
 use actix_web::{Responder, web, post, HttpResponse, HttpRequest};
 use deadpool_postgres::Pool;
+use pct_str::PctStr;
 use serde_json::json;
 
 use crate::error::CodeHarmonyResponseError;
@@ -14,15 +15,15 @@ pub fn init(cfg: &mut web::ServiceConfig){
 #[post("/session/new/{plan_name}/{session_name}")]
 async fn create_session(db_pool: web::Data<Pool>, req: HttpRequest) -> Result<impl Responder,CodeHarmonyResponseError> {
     println!("Creating Session");
-    // Get plan name from uri
+    // Get plan name from uri decoding it as well
     let plan_name = match req.match_info().get("plan_name") {
-        Some(plan_name) => plan_name,
+        Some(plan_name) => PctStr::new(plan_name).map_err(|_| CodeHarmonyResponseError::BadRequest(1,"Bad plan name".to_string()))?.decode(),
         None => return Err(CodeHarmonyResponseError::BadRequest(0,"Expected plan name in uri".to_string()))
     };
 
-    // Get session name from uri
+    // Get session name from uri decoding it as well
     let session_name = match req.match_info().get("session_name") {
-        Some(session_name) => session_name,
+        Some(session_name) => PctStr::new(session_name).map_err(|_| CodeHarmonyResponseError::BadRequest(1,"Bad plan name".to_string()))?.decode(),
         None => return Err(CodeHarmonyResponseError::BadRequest(0,"Expected session name in uri".to_string()))
     };
 
