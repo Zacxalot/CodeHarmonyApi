47d8a83a6fc3e0acd6b64506fa971925b4360fc0
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-01-21 15:21:32 +0000

    Changed props to just a value and added img to ElementType enum

--- src/coding_lesson.rs
+++ src/coding_lesson.rs
@@ -1,6 +1,7 @@
 
 use actix_web::{get, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use crate::jsx_element::{JSXElement, ElementType, JSXChild};
 
 #[derive(Serialize, Deserialize)]
@@ -13,6 +14,6 @@ struct CodingLesson {
 async fn get_coding_lesson() -> impl Responder {
     HttpResponse::Ok().json(CodingLesson {
         lesson_id:1,
-        elements:vec![JSXElement{el_type:ElementType::from_string("h1").unwrap(),props: vec![], children:JSXChild::JSX(vec![])}]
+        elements:vec![JSXElement{el_type:ElementType::from_string("h1").unwrap(),props: json!({}), children:JSXChild::JSX(vec![])}]
     })
 }
--- src/jsx_element.rs
+++ src/jsx_element.rs
@@ -4,7 +4,7 @@ use serde_json::Value;
 #[derive(Deserialize, Serialize,Debug)]
 pub struct JSXElement{
     pub el_type:ElementType,
-    pub props:Vec<Value>,
+    pub props:Value,
     pub children:JSXChild
 }
 
@@ -18,17 +18,17 @@ pub enum JSXChild{
 #[allow(non_camel_case_types)]
 #[derive(Serialize, Deserialize,Debug)]
 pub enum ElementType{
-    div,
     h1,
-    p
+    p,
+    img
 }
 
 impl ElementType{
     pub fn from_string(string: &str) -> Result<ElementType,&str>{
         match string {
-            "div" => return Ok(ElementType::div),
             "h1" => return Ok(ElementType::h1),
             "p" => return Ok(ElementType::p),
+            "img" => return Ok(ElementType::img),
             _ => Err("Invalid element type")
         }
     }
