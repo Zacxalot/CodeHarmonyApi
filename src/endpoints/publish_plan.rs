a62377dc60f535aa11ff83af98301f036a38e8f2
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-04-12 10:31:53 +0100

    Moved search endpoint to publish_plans

--- src/endpoints/publish_plan.rs
+++ src/endpoints/publish_plan.rs
@@ -1,12 +1,15 @@
+use std::convert::TryFrom;
+
 use actix_session::Session;
-use actix_web::{post, web, HttpResponse, Responder};
-use serde::Deserialize;
+use actix_web::{get, post, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 use crate::utils::error::CodeHarmonyResponseError;
 
 // Group all of the services together into a single init
 pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(publish_plan);
+    cfg.service(publish_plan).service(search_plans);
 }
 
 #[derive(Deserialize)]
@@ -106,5 +109,78 @@ async fn publish_plan(
         return Ok(HttpResponse::Ok());
     }
 
+    Err(CodeHarmonyResponseError::NotLoggedIn)
+}
+
+#[derive(Deserialize)]
+struct SearchQuery {
+    s: Option<String>,
+}
+
+#[derive(pg_mapper::TryFromRow, Deserialize, Serialize)]
+struct SearchResult {
+    plan_name: String,
+    username: String,
+    description: String,
+}
+
+#[get("/plan/search")]
+async fn search_plans(
+    query: web::Query<SearchQuery>,
+    db_pool: web::Data<deadpool_postgres::Pool>,
+    session: Session,
+) -> Result<impl Responder, CodeHarmonyResponseError> {
+    // Get username
+    if let Ok(Some(_username)) = session.get::<String>("username") {
+        // Get db client
+        let client = db_pool
+            .get()
+            .await
+            .map_err(|_| CodeHarmonyResponseError::DatabaseConnection)?;
+
+        // Different statements depending on if the query is empty
+        const SEARCH_STATEMENT: &str = "SELECT plan_name, username, description FROM codeharmony.published_lesson_plan plp WHERE (to_tsvector(plan_name) || to_tsvector(username) || to_tsvector(description))  @@ websearch_to_tsquery($1)";
+        const ALL_STATEMENT: &str =
+            "SELECT plan_name, username, description FROM codeharmony.published_lesson_plan";
+
+        let search = &query.into_inner().s.unwrap_or_default();
+
+        // Execute search query
+        let rows = {
+            if search.is_empty() {
+                client.query(ALL_STATEMENT, &[]).await.map_err(|e| {
+                    println!("{:?}", e);
+                    CodeHarmonyResponseError::InternalError(
+                        0,
+                        "Couldn't complete search".to_string(),
+                    )
+                })?
+            } else {
+                client
+                    .query(SEARCH_STATEMENT, &[search])
+                    .await
+                    .map_err(|e| {
+                        println!("{:?}", e);
+                        CodeHarmonyResponseError::InternalError(
+                            0,
+                            "Couldn't complete search".to_string(),
+                        )
+                    })?
+            }
+        };
+
+        // Convert rows into a Vec of SearchResults
+        let results = rows
+            .into_iter()
+            .map(SearchResult::try_from)
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|e| {
+                println!("{:?}", e);
+                CodeHarmonyResponseError::InternalError(0, "Invalid rows".to_string())
+            })?;
+
+        // Return Ok with results!
+        return Ok(HttpResponse::Ok().json(json!(results)));
+    }
     Err(CodeHarmonyResponseError::NotLoggedIn)
 }
